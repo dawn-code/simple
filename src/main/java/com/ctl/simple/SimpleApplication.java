d839cf8b176ae698caadcb447d586071bca1c9b7
chetianliang <chetianliang007@163.com>
2022-03-09 22:04:16 +0800

    测试了 spring aop的jdk动态代理和CGLib的形式

--- src/main/java/com/ctl/simple/SimpleApplication.java
+++ src/main/java/com/ctl/simple/SimpleApplication.java
@@ -2,19 +2,18 @@ package com.ctl.simple;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author ctl
  */
-@EnableSwagger2
-@SpringBootApplication
+//@EnableSwagger2
+//@SpringBootApplication
 @MapperScan("com.ctl.simple.mapper")
 public class SimpleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(SimpleApplication.class, args);
+    }
 
 }
